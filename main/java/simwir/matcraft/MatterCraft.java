f6cf58b350036e996053fd85558bedab2714932e
Simon <Simwir@hotmail.com>
2014-05-14 17:21:07 +0200

    Initial setup

---
+++ main/java/simwir/matcraft/MatterCraft.java
@@ -0,0 +1,56 @@
+package simwir.matcraft;
+
+import net.minecraftforge.common.config.Configuration;
+import simwir.matcraft.lib.References;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = References.MOD_ID, name = References.MOD_NAME, version = References.MOD_VERSION)
+public class MatterCraft {
+	@Instance(References.MOD_ID)
+	public static MatterCraft instance;
+	// Defining blocks
+	//\public static Block waterSource;
+	// Defining Items
+	//\public static Item meter;
+	// Defining other
+	public static boolean debug;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+		debug = config.get("Other", "Debug", false, References.DEBUG_CONFIG_COMMENT).getBoolean(false);
+		config.save();
+		// Adding blocks and items to the actual game
+		// Blocks
+		//\waterSource = new BlockWaterSource();
+		// Items
+		//\meter = new ItemMeter();
+		gameRegisters();
+	}
+	@EventHandler
+	public void init(FMLInitializationEvent event) {}
+	
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {}
+	
+	private static void gameRegisters() {
+
+		// Registers blocks to the game.
+		//\GameRegistry.registerBlock(waterSource, BlockReferences.WATER_SOURCE_UNC_NAME);
+		//Registeres items to the game
+		//\GameRegistry.registerItem(meter, ItemReferences.ITEM_METER_UNC_NAME);
+	}
+	private void registerTileEntities() {
+
+		//\GameRegistry.registerTileEntity(TileSmallTank.class, BlockReferences.SMALL_TANK_TE_KEY);
+	}
+	
+	
+}
---
+++ main/java/simwir/matcraft/lib/References.java
@@ -0,0 +1,10 @@
+package simwir.matcraft.lib;
+
+public class References {
+	public static final String MOD_ID = "matcraft";
+	public static final String MOD_NAME = "Matter Craft";
+	public static final String MOD_VERSION = "pre alfe 0.1";
+	public static final String DEBUG_CONFIG_COMMENT = "Set this to true if the game should run in debug mode, if you don't know what this is leave it at false";
+	public static final String GUI_TEXTURE_LOCATION = "textures/gui/";
+	public static final String CHANNEL = "matcraft";
+}
